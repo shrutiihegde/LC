688d9dabce16491a79b0993787bc8f3afe2cb006
Shruti Hegde <98289103+shrutiihegde@users.noreply.github.com>
2022-11-02 11:41:52 -0400

    Create MergeSortedArraysInPlace.py

---
+++ Day2/MergeSortedArraysInPlace.py
@@ -0,0 +1,27 @@
+"""
+TIme complexity:O(N+M)
+Space complexity: O(1)
+"""
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        #pointers from end
+        p1=m-1
+        p2=n-1
+        
+        #find lesser element and put in place
+        for p in range(n+m-1,-1,-1):
+            if p2<0:
+                break
+            if p1>=0 and nums1[p1]>nums2[p2]:
+                
+                nums1[p]=nums1[p1]
+                p1-=1
+            else:
+                nums1[p]=nums2[p2]
+                p2-=1
+        
+            
+        
